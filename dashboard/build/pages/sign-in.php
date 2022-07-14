cf7628a30474bcb80d998a63f5992a9cb740a1be
ericsoi <ericksoi3709@gmail.com>
2022-07-14 10:10:11 +0300

    login

--- dashboard/build/pages/sign-in.php
+++ dashboard/build/pages/sign-in.php
@@ -80,7 +80,7 @@
                                     <p class="mb-0">Enter your email/phone and password to sign in</p>
                                 </div>
                                 <div class="flex-auto p-6">
-                                    <form role="form" action="../.../processor/handle_login.php" method="POST">
+                                    <form role="form" action="../../processor/handle_login.php" method="POST">
                                         <label class="mb-2 ml-1 font-bold text-size-xs text-slate-700">Email</label>
                                         <div class="mb-4">
                                             <input type="text" id="username", name="username", class="focus:shadow-soft-primary-outline text-size-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
