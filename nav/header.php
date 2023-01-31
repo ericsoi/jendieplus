6895e47811a51a64f780125d29e06cb0388abb92
eric.soi <erick.soi@hotmail.com>
2023-01-31 15:16:45 +0300

    google annalitics

--- nav/header.php
+++ nav/header.php
@@ -153,6 +153,15 @@
     100% { transform: translate(-50%, -50%) rotate(360deg); }
 }
 </style>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-4YGDSH48QX"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-4YGDSH48QX');
+</script>
 </head>
 
 <body>
