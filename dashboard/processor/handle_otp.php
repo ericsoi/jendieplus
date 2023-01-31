91a0e064935095b2ec4afe59c3447a8b6089b6b8
eric.soi <erick.soi@hotmail.com>
2023-01-31 18:56:45 +0300

    handle otp

--- dashboard/processor/handle_otp.php
+++ dashboard/processor/handle_otp.php
@@ -3,8 +3,7 @@ if (session_status() == PHP_SESSION_NONE) {
 	session_start();
 }
 if(isset($_POST['btn_otp'])){
-    if(1==1){
-    // if($_POST['otp'] == $_SESSION['otp']){
+    if($_POST['otp'] == $_SESSION['otp']){
         header ("Location: ../build/pages/dashboard.php?q=".$_SESSION["username"]);
     }else{
         header ("Location: ../build/pages/otp.php?status=error");
